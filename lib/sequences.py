39fd8aefce8f3531d2d3361f2f68e4ddce28d06c
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-27 19:00:43 +0300

    fix: Define print-fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0,1])
+    else:
+        fib = [0,1]
+        count = 2
+        while count < length:
+            next_element = fib[-1] + fib[-2]
+            fib.append(next_element)
+            count += 1
+        print(fib)
+ 
